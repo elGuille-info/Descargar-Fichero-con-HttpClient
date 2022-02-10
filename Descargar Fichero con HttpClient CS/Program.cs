3f88a98de94cc90f403bfc80a959a1a7fe269e89
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2022-02-10 19:37:32 +0100

    Add project files.

---
+++ Descargar Fichero con HttpClient CS/Program.cs
@@ -0,0 +1,89 @@
+﻿//--------------------------------------------------------------------------------
+// Descargar un fichero de un sitio web usando HttpClient        (10/Feb/22 19.25)
+//
+// (c) Guillermo Som (Guille), 2022
+//--------------------------------------------------------------------------------
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Descargar_Fichero_con_HttpClient_CS
+{
+    class Program
+    {
+        /// <summary>
+        /// El objeto HttpClient se recomiendo instanciarlo solo 1 vez en la aplicación.
+        /// </summary>
+        private readonly static System.Net.Http.HttpClient ClienteHttp = new System.Net.Http.HttpClient();
+
+        // En C# 7.1 y superior se puede usar Main como Task y async.
+        static async Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var ficWeb = "https://www.elguille.info/pruebaGuille.txt";
+            var ficLocal = "prueba.txt";
+
+            Console.WriteLine("Descargando {0}...", ficWeb);
+
+            var res = await DownloadFileAsync(ficWeb, ficLocal);
+            if (res)
+            {
+                Console.WriteLine("Descarga completada.");
+
+                // Mostrar el contenido del fichero local.
+                Process.Start("notepad", ficLocal);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Pulsa INTRO para finalizar.");
+
+
+
+            // Las versiones de C# anteriores a 7.1 no pueden esperar en Main,
+            // por tanto, el código anterior ponerlo en un método y llamarlo desde aquí
+            // y esperar a que se termine todo...
+            //descargar();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Descarga el fichero indicado (url) y lo guarda en el fichero destino (usando HttpClient).
+        /// </summary>
+        /// <param name="ficWeb">El fichero a descargar (de una dirección URL).</param>
+        /// <param name="ficDest">El fichero de destino, donde se guardará el descargado.</param>
+        public async static Task<bool> DownloadFileAsync(string ficWeb, string ficDest)
+        {
+            try
+            {
+                // Simplificando la descarga.
+                var contenido = await ClienteHttp.GetByteArrayAsync(ficWeb);
+                // Si se ha podido descargar.
+                if (contenido != null && contenido.Length > 0)
+                {
+                    // Guardarlo en el fichero de destino.
+                    // Si el fichero destino existe, se sobreescribe.
+                    using (System.IO.FileStream fs = new System.IO.FileStream(ficDest, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None))
+                    {
+                        await fs.WriteAsync(contenido.AsMemory(0, contenido.Length));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No se ha podido descargar.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Se ha producido un error al descargar o guardar.
+                Console.WriteLine("Error: {0}", ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Descargar Fichero con HttpClient VB/Program.vb
@@ -0,0 +1,74 @@
+'--------------------------------------------------------------------------------
+' Descargar un fichero de un sitio web usando HttpClient        (10/Feb/22 19.05)
+'
+' (c) Guillermo Som (Guille), 2022
+'--------------------------------------------------------------------------------
+Imports System
+Imports System.Diagnostics
+Imports System.Threading.Tasks
+
+Module Program
+
+    ''' <summary>
+    ''' El objeto HttpClient se recomiendo instanciarlo solo 1 vez en la aplicación.
+    ''' </summary>
+    Private ReadOnly ClienteHttp As New System.Net.Http.HttpClient()
+
+    Sub Main(args As String())
+        'Console.WriteLine("Hello World!")
+
+        ' Como en VB no se puede esperar en Main,
+        ' hacer el trabajo asíncrono en otro método y esperar a que se termine todo...
+        descargar()
+
+        Console.ReadLine()
+    End Sub
+
+    Private Async Sub descargar()
+        Dim ficWeb = "https://www.elguille.info/pruebaGuille.txt"
+        Dim ficLocal = "prueba.txt"
+
+        Console.WriteLine("Descargando {0}...", ficWeb)
+
+        Dim res = Await DownloadFileAsync(ficWeb, ficLocal)
+        If res Then
+            Console.WriteLine("Descarga completada.")
+
+            ' Mostrar el contenido del fichero local.
+            Process.Start("notepad", ficLocal)
+        End If
+
+        Console.WriteLine()
+        Console.WriteLine("Pulsa INTRO para finalizar.")
+    End Sub
+
+    ''' <summary>
+    ''' Descarga el fichero indicado (url) y lo guarda en el fichero destino (usando HttpClient).
+    ''' </summary>
+    ''' <param name="ficWeb">El fichero a descargar (de una dirección URL).</param>
+    ''' <param name="ficDest">El fichero de destino, donde se guardará el descargado.</param>
+    Public Async Function DownloadFileAsync(ficWeb As String, ficDest As String) As Task(Of Boolean)
+        Try
+            ' Simplificando la descarga.
+            Dim contenido = Await ClienteHttp.GetByteArrayAsync(ficWeb)
+            ' Si se ha podido descargar.
+            If contenido IsNot Nothing AndAlso contenido.Length > 0 Then
+                ' Guardarlo en el fichero de destino.
+                ' Si el fichero destino existe, se sobreescribe.
+                Using fs As New System.IO.FileStream(ficDest, System.IO.FileMode.Create, System.IO.FileAccess.Write, System.IO.FileShare.None)
+                    Await fs.WriteAsync(contenido.AsMemory(0, contenido.Length))
+                End Using
+            Else
+                Console.WriteLine("No se ha podido descargar.")
+                Return False
+            End If
+        Catch ex As Exception
+            ' Se ha producido un error al descargar o guardar.
+            Console.WriteLine("Error: {0}", ex.Message)
+            Return False
+        End Try
+
+        Return True
+    End Function
+
+End Module
